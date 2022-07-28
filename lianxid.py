bc5ad4b5d3aef2b6ba6c939b344838c7ee54b9f1
huihui <1517003117@qq.com>
2022-07-28 13:41:21 +0800

    ceshiyixiiii-----

--- lianxid.py
+++ lianxid.py
@@ -31,5 +31,18 @@ else:
     print("账号长度不符合")
 
 
+username=input("请输入账号：")
+password=input("请输入密码：")
+if len(username) >=5 and len(username) <=8:
+    if username[0] in "qwertyuiopasdfghjklzxcvbnm":
+        if  len(password)>=8 and len(password) <=  12:
+            print("注册成功！",{username,password})
+        else:
+            print("密码必须8-12位！")
+    else:
+        print("账号首字母必须小写开头！")
+else:
+    print("账号长度不符合")
+
 
     
